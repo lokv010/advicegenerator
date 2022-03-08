73885fa8b7388f564b50516a23cd687017c331aa
lokv010 <lokv.010@gmail.com>
2022-03-08 01:47:08 -0500

    Advice-generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+
+export interface quote {
+  id: number;
+  advice: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'advicegenerator';
+  qot$!:quote;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getQuote();
+  }
+
+  getQuote() {
+    this.http
+      .get<any>('https://api.adviceslip.com/advice').pipe(map(res=> res['slip']))
+      .subscribe((data) => {
+        this.qot$ = data;
+        console.log(this.qot$);
+      });
+  }
 }
